e40e8cd7309be4ff84827f768d0a29dd9fc88fd8
Chris K <Chris-SourceCode@outlook.com>
2021-10-15 00:01:26 +0100

    chore: Remove not-needed files

--- dotNet-challenges/Challenge1.cs
+++ dotNet-challenges/Challenge1.cs
@@ -1,15 +1,25 @@
 ﻿
-using dotNet_challenges;
+namespace dotNet_challenges;
 
-var counter = 0;
-var currentNum = 0;
-while (currentNum <= 100)
+public static class Prime
 {
-    if (PrimeHelper.IsPrime(currentNum))
-    {
-        counter++;
-    }
-    currentNum++;
-}
 
-Console.WriteLine($"There are {counter} Prime Numbers in the range from 0 to 100");
+    public static void CountPrimesInRange()
+    {
+        var counter = 0;
+
+        var currentNum = 0;
+        while (currentNum <= 100)
+        {
+            if (Helpers.IsPrime(currentNum))
+            {
+                counter++;
+            }
+
+            currentNum++;
+        }
+
+        Console.WriteLine(
+            $"There are {counter} Prime Numbers in the range from 0 to 100");
+    }
+}
